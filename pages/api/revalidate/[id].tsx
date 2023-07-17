578c08cfc9e1bd26bf3f976d4401d40380c10c98
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-07-17 16:20:17 +0900

    バッククオートに変更

--- pages/api/revalidate/[id].tsx
+++ pages/api/revalidate/[id].tsx
@@ -9,13 +9,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  console.log('Revalidateing detail page...')
+  console.log('Revalidating detail page...')
   const {
     query: { id },
   } = req
   let revalidated = false
   try {
-    await res.revalidate('/notes/${id}')
+    await res.revalidate(`/notes/${id}`)
     revalidated = true
   } catch (err) {
     console.log(err)
